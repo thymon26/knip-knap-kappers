24fc5d98e18d28cc20b7b73aa6e3e4885f15e9d5
thymon26 <djthymen1@gmail.com>
2025-05-16 09:57:47 +0200

    Update db.php

--- db.php
+++ db.php
@@ -10,7 +10,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     // Set error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Database connection successful.";
 } catch (PDOException $e) {
     // Handle connection error
     die("Database connection failed: " . $e->getMessage());
