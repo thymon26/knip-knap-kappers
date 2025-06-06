328e0882245a88e72efd8e84bd4b46850fd92878
thymon26 <djthymen1@gmail.com>
2025-06-06 16:03:43 +0200

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -4,35 +4,6 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 require 'db.php';
 
-// Zoek adres in database (maar één keer definiëren!)
-function zoekAdres($postcode, $huisnummer, $pdo) {
-    $postcode = strtoupper(str_replace(' ', '', $postcode));
-    if (strlen($postcode) === 6) {
-        $postcode = substr($postcode, 0, 4) . ' ' . substr($postcode, 4, 2);
-    }
-    $stmt = $pdo->prepare("SELECT straat, plaats FROM adressen WHERE postcode = ? AND huisnummer = ?");
-    $stmt->execute([$postcode, $huisnummer]);
-    return $stmt->fetch(PDO::FETCH_ASSOC);
-}
-
-// AJAX-adreslookup
-if (
-    isset($_POST['ajax']) && $_POST['ajax'] == '1'
-    && isset($_POST['postcode']) && isset($_POST['huisnummer'])
-) {
-    header('Content-Type: application/json');
-    try {
-        $adres = zoekAdres($_POST['postcode'], $_POST['huisnummer'], $pdo);
-        if ($adres) {
-            echo json_encode(['straat' => $adres['straat'], 'woonplaats' => $adres['plaats']]);
-        } else {
-            echo json_encode(['straat' => '', 'woonplaats' => '']);
-        }
-    } catch (Throwable $e) {
-        echo json_encode(['straat' => '', 'woonplaats' => '', 'error' => $e->getMessage()]);
-    }
-    exit;
-}
 
 // Laad PHPMailer
 require 'phpmailer/src/PHPMailer.php';
